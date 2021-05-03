bee6c3a3281263713ec172b7259bf9445ad0e7e9
tom <82261837+leetingjiu@users.noreply.github.com>
2021-05-03 17:12:21 +0800

    Create profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,14 @@
+#this is main code
+$Host.UI.RawUI.WindowTitle="Tom"
+$colors = @{}
+$colors['Variable'] = [System.ConsoleColor]::DarkCyan
+$colors['Member'] = [System.ConsoleColor]::Blue
+$colors['Command'] = [System.ConsoleColor]::Blue
+$colors['Number'] = [System.ConsoleColor]::DarkGreen
+$colors['None'] = [System.ConsoleColor]::Black
+$colors['Parameter'] = [System.ConsoleColor]::DarkGray
+$colors['Keyword'] = [System.ConsoleColor]::Red
+$colors['Type'] = [System.ConsoleColor]::DarkBlue
+$colors['String'] = [System.ConsoleColor]::DarkGreen
+$colors['Operator'] = [System.ConsoleColor]::DarkGray
+Set-PSReadLineOption -Colors $colors
